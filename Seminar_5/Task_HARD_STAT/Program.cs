eff769f12b4ca378fbf69f9beb4c720c5e88bd1f
Вадим <118062456+DeveloperVadim@users.noreply.github.com>
2022-12-09 01:53:12 +0700

    Hard Stat

--- Seminar_5/Task_HARD_STAT/Program.cs
+++ Seminar_5/Task_HARD_STAT/Program.cs
@@ -4,8 +4,21 @@
 // на экран с пояснениями. Найти медианное значение первоначалального массива , 
 // возможно придется кое-что для этого дополнительно выполнить.
 
+void PrintArr2(int[] array)
+{
+
+    Console.Write("Массив2: ");
+    foreach (int el in array)
+    {
+        Console.Write($"{el} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Мин знач.:{array[0]} Мин инд.:{array[1]} Макс знач.:{array[2]} Макс инд.:{array[3]} Ср. арефм.:{array[4]}");
+}
+
 void PrintArr(int[] array)
 {
+
     Console.Write("Массив: ");
     foreach (int el in array)
     {
@@ -22,7 +35,7 @@ int[] RandNum(int len)
     return array;
 }
 
-void FindSumUnev(int[] array, int len)
+int[] FindArr2(int[] array, int len)
 {
     int min = array[0];
     int minI = 0;
@@ -45,36 +58,41 @@ void FindSumUnev(int[] array, int len)
         }
     }
     sum = sum / len;
-    Console.WriteLine();
-    Console.WriteLine($"Мин знач.:{min} Мин инд.:{minI} Макс знач.:{max} Макс инд.:{maxI} Ср. арефм.:{sum}");
+    int[] array2 = new int[5];
+    array2[0] = min;
+    array2[1] = minI;
+    array2[2] = max;
+    array2[3] = maxI;
+    array2[4] = sum;
+    return array2;
 }
 
-void SelectionSort(int[] array,int len) 
+void SelectionSort(int[] array, int len)
 {
     for (int i = 0; i < len - 1; i++)
     {
-        int minPosition = i; 
-        for (int j = i + 1; j < len; j++) 
+        int minPosition = i;
+        for (int j = i + 1; j < len; j++)
         {
-            if (array[j] < array[minPosition]) 
+            if (array[j] < array[minPosition])
             {
                 minPosition = j;
             }
         }
-        int temporary = array[i]; 
+        int temporary = array[i];
         array[i] = array[minPosition];
         array[minPosition] = temporary;
     }
 }
 
-int MedianNum(int[] array,int len)
+int MedianNum(int[] array, int len)
 {
-    int median=0;
+    int median = 0;
     for (int i = 0; i < len - 1; i++)
     {
-        if (len%2==1)median=array[(len-1)/2]; // нечетная длина массива
-        if (len%2==0)median=(array[len/2] +array[(len/2)-1])/2; // четная длина массива
-       
+        if (len % 2 == 1) median = array[(len - 1) / 2]; // нечетная длина массива
+        if (len % 2 == 0) median = (array[len / 2] + array[(len / 2) - 1]) / 2; // четная длина массива
+
     }
     return median;
 }
@@ -82,9 +100,13 @@ int MedianNum(int[] array,int len)
 Console.WriteLine("Задайте длину массива:");
 int len = Convert.ToInt32(Console.ReadLine());
 int[] array = RandNum(len);
+int[] array2 = FindArr2(array, len);
 PrintArr(array);
-FindSumUnev(array, len);
+Console.WriteLine();
+//FindArr2(array, len);
 SelectionSort(array, len);
 PrintArr(array);
 Console.WriteLine();
- Console.WriteLine($"Медианное значение: {MedianNum(array,len)}");
+Console.WriteLine($"Медианное значение: {MedianNum(array, len)}");
+PrintArr2(array2);
+Console.WriteLine();
