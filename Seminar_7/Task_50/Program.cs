8bf231a85a63961649483fabb0f739b6c9b44e56
Вадим <118062456+DeveloperVadim@users.noreply.github.com>
2022-12-17 17:10:16 +0700

    Task 50

--- Seminar_7/Task_50/Program.cs
+++ Seminar_7/Task_50/Program.cs
@@ -7,4 +7,46 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-345
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t"); 
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 11);
+}
+
+void SearchNum(int[,] array, int num)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (num == array[i, j])
+            {
+                Console.WriteLine($"Строка: {i+1} Столбец: {j+1}");
+                count++;
+            }
+        }
+    if (count == 0) Console.WriteLine($"{num} Число отсутствует в массиве.");
+}
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите искомое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SearchNum(array, num);
