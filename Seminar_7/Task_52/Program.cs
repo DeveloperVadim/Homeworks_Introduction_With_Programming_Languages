2d88191ab1f38e2c8c79b19a9c0eb7e83b9ebdd7
Вадим <118062456+DeveloperVadim@users.noreply.github.com>
2022-12-17 18:40:10 +0700

    Task 50

--- Seminar_7/Task_52/Program.cs
+++ Seminar_7/Task_52/Program.cs
@@ -6,4 +6,44 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-345
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 11);
+}
+
+void AverageCols(int[,] array, int[] sum) // Доработать 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+          
+        for (int j = 0; j < array.GetLength(1); j++)
+            sum[i] += array[j, i];
+
+    Console.Write("Среднее арефметическое каждого столбца: ");
+    foreach (double elem in sum)
+    {
+        Console.Write($"{Math.Round(elem / array.GetLength(1), 1)}   ");
+    }
+}
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+int[] sum = new int[array.GetLength(1)];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+AverageCols(array, sum);
